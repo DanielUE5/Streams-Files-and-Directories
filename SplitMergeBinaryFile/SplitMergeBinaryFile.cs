15b0bac70dff1f9ba349951c199ab3e3643a9e4b
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2025-10-25 03:50:42 +0300

    Add project files.

---
+++ SplitMergeBinaryFile/SplitMergeBinaryFile.cs
@@ -0,0 +1,54 @@
+﻿namespace SplitMergeBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            byte[] fileData = File.ReadAllBytes(sourceFilePath);
+            int totalBytes = fileData.Length;
+
+            int partOneSize = (totalBytes + 1) / 2; 
+            int partTwoSize = totalBytes / 2;
+
+            using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Create, FileAccess.Write))
+            {
+                partOneStream.Write(fileData, 0, partOneSize);
+            }
+
+            using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Create, FileAccess.Write))
+            {
+                partTwoStream.Write(fileData, partOneSize, partTwoSize);
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream joinedStream = new FileStream(joinedFilePath, FileMode.Create, FileAccess.Write))
+            {
+                using (FileStream partOneStream = new FileStream(partOneFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    partOneStream.CopyTo(joinedStream);
+                }
+
+                using (FileStream partTwoStream = new FileStream(partTwoFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    partTwoStream.CopyTo(joinedStream);
+                }
+            }
+        }
+    }
+}
---
+++ WordCount/WordCount.cs
@@ -0,0 +1,65 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader wordsReader = new StreamReader(wordsFilePath))
+            {
+                using (StreamReader textReader = new StreamReader(textFilePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        List<string> wordsList = wordsReader
+                            .ReadToEnd()
+                            .ToLower()
+                            .Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+                            .ToList();
+
+                        string[] textWords = textReader
+                            .ReadToEnd()
+                            .ToLower()
+                            .Split(new char[] { ' ', '\n', '\r', '.', ',', '!', '?', ';', ':', '-', '…' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+                        foreach (var word in wordsList)
+                        {
+                            if (!wordCounts.ContainsKey(word))
+                            {
+                                wordCounts[word] = 0;
+                            }
+                        }
+
+                        foreach (var word in textWords)
+                        {
+                            if (wordCounts.ContainsKey(word))
+                            {
+                                wordCounts[word]++;
+                            }
+                        }
+
+                        foreach (var kvp in wordCounts.OrderByDescending(x => x.Value))
+                        {
+                            writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
